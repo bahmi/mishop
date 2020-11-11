958ba71aaa3a426a0153056dfc49c4b1d915c13d
Bahadur Alam <bahadurmisho@gmail.com>
2020-11-11 12:23:13 +0600

    Implement user registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,9 +27,23 @@ const userSchema = mongoose.Schema(
   }
 );
 
+// check if the user entered password matches
+// with the one in the database
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// encrypt the user password
+userSchema.pre("save", async function (next) {
+  // check if the field has been modified
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // if modified, then hash it
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
