2eaf9230558c2c51dd4753c83a41b8ca80f8a6a0
Bahadur Alam <bahadurmisho@gmail.com>
2020-11-12 13:37:11 +0600

    Create shipping page

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,13 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     // handle if an item already exists in the cart
     case CART_ADD_ITEM:
@@ -26,6 +33,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         ...state,
         // strip out the item
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
       };
     default:
       return state;
