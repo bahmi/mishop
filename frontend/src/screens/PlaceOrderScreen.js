abcc3a78dc66057c57c9254611afffeb334500be
Bahadur Alam <bahadurmisho@gmail.com>
2020-11-16 23:22:20 +0600

    Fix cart length error in place order page

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -77,7 +77,7 @@ const PlaceOrderScreen = ({ history }) => {
 
             <ListGroup.Item>
               <h2>Order Items</h2>
-              {cart.length === 0 ? (
+              {cart.cartItems.length === 0 ? (
                 <Message>Your cart is empty</Message>
               ) : (
                 <ListGroup variant="flush">
