15cc48edad941b7878c0fec53dfd3bc2192a101d
Bahadur Alam <bahadurmisho@gmail.com>
2021-01-21 21:57:18 +0600

    Rename brand

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,7 +12,7 @@ const Meta = ({ title, description, keywords }) => {
 };
 
 Meta.defaultProps = {
-  title: "Welcome to ProShop",
+  title: "Welcome to MiShop",
   description: "We sell the best products for cheap",
   keywords: "electronics, buy electronics, sell electronics",
 };
